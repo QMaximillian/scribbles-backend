db3b1b114b011b6b517b99a4d74a634462c8be21
QMaximillian <quinnlashinsky@gmail.com>
2019-02-10 14:31:38 -0500

    Commit

--- db/migrate/20190110183518_create_users.rb
+++ db/migrate/20190110183518_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email, limit: 35
-      t.integer :meeting_range_id
+      t.integer :meeting_range_id, optional: true
       t.string :first_name
       t.string :last_name
       t.boolean :admin
